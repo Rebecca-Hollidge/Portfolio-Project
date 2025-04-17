5d418e1fb966c1df60a041cdef9302eb81722dcb
rebecca hollidge <147550671+Rebecca-Hollidge@users.noreply.github.com>
2025-04-17 18:53:47 +0100

    layout updates

---
+++ project/svelte-app/src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+    // Import components
+    import { Header, Footer} from "$lib";
+</script>
+
+
+<Header />
+<!-- Page Content -->
+<main>
+    <slot />
+</main>
+
+<Footer />
+
+<style>
+    main {
+        padding: 2rem;
+        color: white;
+        text-align: center;
+        background-color: #ffb8f3;
+        min-height: calc(100vh - 308px);
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    }
+
+</style>
