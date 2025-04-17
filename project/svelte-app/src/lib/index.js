e6e7ed0e4ff298e3f9758f10c734a6a9fbc67443
rebecca hollidge <147550671+Rebecca-Hollidge@users.noreply.github.com>
2025-04-17 18:15:20 +0100

    webpages

--- project/svelte-app/src/lib/index.js
+++ project/svelte-app/src/lib/index.js
@@ -5,4 +5,3 @@ export { default as Header } from "./components/Header.svelte";
 export { default as Navigation } from "./components/Navigation.svelte";
 export { default as Footer } from "./components/Footer.svelte";
 export { default as Card } from "./components/Card.svelte";
-export { default as Form } from "./components/Form.svelte";
