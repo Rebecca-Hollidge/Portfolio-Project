dbd812da68d869cfba3a57ccff74f74571743f67
rebecca hollidge <147550671+Rebecca-Hollidge@users.noreply.github.com>
2025-04-22 21:47:03 +0100

    secret cat page

---
+++ project/svelte-app/src/routes/cat/+page.svelte
@@ -0,0 +1,33 @@
+
+<script>
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        console.log("about Page Loaded");
+    });
+</script>
+
+
+
+
+<div>
+    <h1> mewmew </h1>
+    <img src = "images/mew.jpg"alt="mew" >
+    <img src = "images/mew2.jpg"alt="mew2" >
+</div>
+<div>
+    <h1> yam cat </h1>
+    <img src = "images/yam.jpg"alt="yam" >
+    <img src = "images/yam2.jpg"alt="yam2" >
+</div>
+<style>
+
+    img {
+    
+        height: 20%;
+        width: 20%;
+        padding : 10px;
+    }
+    
+    
+            </style>
